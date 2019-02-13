91192b81aab4bf6db6900954e1c7aabc37452069
John71570 <john_lyon@hotmail.fr>
2019-02-13 22:30:49 +0100

    Les courses avancent

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-	secret: "key"
+	secret: "key",
+	cookie: { maxAge: 10000000 }
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
