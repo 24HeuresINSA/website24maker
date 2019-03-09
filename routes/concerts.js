96f3f14bdf61099d9111f2c0c98a37aa09b10772
John71570 <john_lyon@hotmail.fr>
2019-03-09 13:49:53 +0100

    Update

--- routes/concerts.js
+++ routes/concerts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/timetable', function(req, res, next) {
-	res.render('concerts-timetable2');
+	res.render('concerts-timetable');
 });
 
 router.get('/artistes', function(req, res, next) {
