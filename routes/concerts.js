6005fa4898b8e9984451bfd14e90f2be58f15f15
John71570 <john_lyon@hotmail.fr>
2019-03-17 00:01:34 +0100

    Update design

--- routes/concerts.js
+++ routes/concerts.js
@@ -6,7 +6,7 @@ router.get('/timetable', function(req, res, next) {
 });
 
 router.get('/artistes', function(req, res, next) {
-	res.render('concerts-artistes-2019');
+	res.render('concerts-artistes-2019-nuit');
 });
 
 router.get('/tremplins', function(req, res, next) {
