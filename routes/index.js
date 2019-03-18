885a15f46bfbf61357bf067c1c031c9275a9561d
John71570 <john_lyon@hotmail.fr>
2019-03-19 00:01:07 +0100

    Update orga benevole

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/c==3', function(req, res, next) {
 	res.redirect('https://www.forumorg.org/');
 });
 
+router.get('/orga', function(req, res, next){
+	res.redirect('http://assomaker.24heures.org/login/token/85365b7c91dae190208fa7d7afd990a1?fbclid=IwAR3u8lu_-bKvo2X0H04A0Wp3gepGbsZPMRIU9IVKJ0dThdpoyK_pSZC3s2g');
+});
+
 router.get('/partenaires', function(req, res, next) {
 	res.render('a-propos-partenaires');
 });
