c9d9366c89cee7e300cf276688439890a9196f2e
Titouan-Joseph <titouanjoseph@gmail.com>
2021-08-04 19:32:32 +0200

    feat(update2021): 🎡 anims history

--- routes/animations.js
+++ routes/animations.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.render('animations-animations');
+	res.render('animations-animations-old');
 });
 
 router.get('/plan', function(req, res, next){
